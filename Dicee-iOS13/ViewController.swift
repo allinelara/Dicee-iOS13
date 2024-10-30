8289d3014cb6dda8c82950dabd0b8c79cda84d34
allinelara <allinelara@gmail.com>
2024-10-30 09:45:54 +0100

    create roll dice app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceViewImageOne: UIImageView!
+    
+    @IBOutlet weak var diceViewImageTwo: UIImageView!
+    
+
+    @IBAction func rollButton(_ sender: Any) {
+        
+        let images = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo") , UIImage(named: "DiceThree"), UIImage(named: "DiceFour"),UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+         
+        diceViewImageOne.image =  images[Int.random(in: 0...5)]
+        diceViewImageTwo.image =  images[Int.random(in: 0...5)]
+        
     }
-
-
 }
 
